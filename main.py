ea106027c9468707cb27c8bddb8eae467805e78c
Benji <BenPI@email.com>
2024-10-05 10:36:43 -0500

    jfkdls;afjklds;afhadjk;fjsdkla;fdjksal;fjdksal;fjkdls;a

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import discord, asyncio
+client = discord.Client(intents=discord.Intents.all())
+tree = discord.app_commands.CommandTree
 
+@client.event
+async def on_join(guild):
+    try
+    for i in guild.channels
